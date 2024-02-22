efcefafa6d4b61bdda16b97104e66a101a1a6e3c
William Brendel <brendel.william@gmail.com>
2024-02-21 16:48:17 -0800

    components

--- src/core/components/index.js
+++ src/core/components/index.js
@@ -1,3 +1,16 @@
+import atoms from './atoms';
+import modals from './modals';
+import molecules from './molecules';
+import navigation from './navigation';
+import organisms from './organisms';
+import templates from './templates';
+
+// Exports.
 export default {
-  
+  ...atoms,
+  ...modals,
+  ...molecules,
+  ...navigation,
+  ...organisms,
+  ...templates
 }
