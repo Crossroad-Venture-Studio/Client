3613f2e1864a401e97c713e998f54360984a9609
William Brendel <brendel.william@gmail.com>
2024-03-14 17:33:01 -0700

    client submodule

--- src/core/components/atoms/LinkLine.js
+++ src/core/components/atoms/LinkLine.js
@@ -39,37 +39,36 @@ export const LinkLine = props => {
   Array.isArray(children || (children = [])) || (children = [children]);
 
   // Layout.
-  return <div className={className} {...other}>
-    {(rightText) && <div>
-      {(leftSrc || leftText) && <div title={leftTitle || leftAlt || null}>
-        {leftSrc && <img src={leftSrc} className='icon' alt={leftAlt || leftTitle || null} /> || null}
-        {leftText && <span className='fat vertical-trim'>{leftText}</span> || null}
-      </div> || null}
-      {...children}
-      <a
-        href={href || null}
-        {...events}
-        title={rightTitle || rightAlt || null}
-      >
-        <span className='fat vertical-trim'>{rightText}</span>
-        {rightSrc && <img src={rightSrc} className='icon' alt={rightAlt || rightTitle || null} /> || null}
-      </a>
-    </div> || <a
+  return rightText && <div className={className} {...other}>
+    {(leftSrc || leftText) && <div title={leftTitle || leftAlt || null}>
+      {leftSrc && <img src={leftSrc} className='icon' alt={leftAlt || leftTitle || null} /> || null}
+      {leftText && <span className='fat vertical-trim'>{leftText}</span> || null}
+    </div> || null}
+    {...children}
+    <a
       href={href || null}
       {...events}
+      title={rightTitle || rightAlt || null}
     >
-      {(leftSrc || leftText) && <div title={leftTitle || leftAlt || null}>
-        {leftSrc && <img src={leftSrc} className='icon' alt={leftAlt || leftTitle || null} /> || null}
-        {leftText && <span className='fat vertical-trim'>{leftText}</span> || null}
-      </div> || null}
-      {...children}
-      {(rightSrc || rightText) && <div
-        title={rightTitle || rightAlt || null}
-      >
-        {rightText && <span className='fat vertical-trim'>{rightText}</span> || null}
-        {rightSrc && <img src={rightSrc} className='icon' alt={rightAlt || rightTitle || null} /> || null}
-      </div> || null}
-    </a>}
+      <span className='fat vertical-trim'>{rightText}</span>
+      {rightSrc && <img src={rightSrc} className='icon' alt={rightAlt || rightTitle || null} /> || null}
+    </a>
+  </div> || <div
+    className={className}
+    href={href || null}
+    {...events}
+    {...other}
+  >
+    {(leftSrc || leftText) && <div title={leftTitle || leftAlt || null}>
+      {leftSrc && <img src={leftSrc} className='icon' alt={leftAlt || leftTitle || null} /> || null}
+      {leftText && <span className='fat vertical-trim'>{leftText}</span> || null}
+    </div> || null}
+    {...children}
+    <div
+      title={rightTitle || rightAlt || null}
+    >
+      {rightSrc && <img src={rightSrc} className='icon' alt={rightAlt || rightTitle || null} title={rightTitle || rightAlt || null} /> || null}
+    </div>
   </div>
 }
 
