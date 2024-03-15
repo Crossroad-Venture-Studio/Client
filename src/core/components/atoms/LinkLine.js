f99fbf47e1989130d54491f04602830c8d6069a0
William Brendel <brendel.william@gmail.com>
2024-03-14 17:17:28 -0700

    client submodule

--- src/core/components/atoms/LinkLine.js
+++ src/core/components/atoms/LinkLine.js
@@ -40,19 +40,36 @@ export const LinkLine = props => {
 
   // Layout.
   return <div className={className} {...other}>
-    {(leftSrc || leftText) && <div title={leftTitle || leftAlt || null}>
-      {leftSrc && <img src={leftSrc} className='icon' alt={leftAlt || leftTitle || null} /> || null}
-      {leftText && <span className='fat vertical-trim'>{leftText}</span> || null}
-    </div> || null}
-    {...children}
-    {(rightSrc || rightText) && <a
-      title={rightTitle || rightAlt || null}
+    {(rightText) && <a
       href={href || null}
       {...events}
     >
-      {rightText && <span className='fat vertical-trim'>{rightText}</span> || null}
-      {rightSrc && <img src={rightSrc} className='icon' alt={rightAlt || rightTitle || null} /> || null}
-    </a> || null}
+      {(leftSrc || leftText) && <div title={leftTitle || leftAlt || null}>
+        {leftSrc && <img src={leftSrc} className='icon' alt={leftAlt || leftTitle || null} /> || null}
+        {leftText && <span className='fat vertical-trim'>{leftText}</span> || null}
+      </div> || null}
+      {...children}
+      <div
+        title={rightTitle || rightAlt || null}
+      >
+        <span className='fat vertical-trim'>{rightText}</span>
+        {rightSrc && <img src={rightSrc} className='icon' alt={rightAlt || rightTitle || null} /> || null}
+      </div>
+    </a> || <>
+      {(leftSrc || leftText) && <div title={leftTitle || leftAlt || null}>
+        {leftSrc && <img src={leftSrc} className='icon' alt={leftAlt || leftTitle || null} /> || null}
+        {leftText && <span className='fat vertical-trim'>{leftText}</span> || null}
+      </div> || null}
+      {...children}
+      {(rightSrc || rightText) && <a
+        title={rightTitle || rightAlt || null}
+        href={href || null}
+        {...events}
+      >
+        {rightText && <span className='fat vertical-trim'>{rightText}</span> || null}
+        {rightSrc && <img src={rightSrc} className='icon' alt={rightAlt || rightTitle || null} /> || null}
+      </a> || null}
+    </>}
   </div>
 }
 
