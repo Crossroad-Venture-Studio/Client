49680c7322706efdede7e925e1fa1e63d4be562e
William Brendel <brendel.william@gmail.com>
2024-03-14 22:22:10 -0700

    client submodule

--- src/core/components/atoms/index.js
+++ src/core/components/atoms/index.js
@@ -6,3 +6,4 @@ export { default as Form } from './Form';
 export { default as NextButton } from './NextButton';
 export { default as Pill } from './Pill';
 export { default as Row } from './Row';
+export { default as SubmitButton } from './SubmitButton';
