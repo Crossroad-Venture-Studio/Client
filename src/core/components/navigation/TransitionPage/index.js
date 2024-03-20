67310ead011486f68dbc338ff0e04f259048e2de
William Brendel <brendel.william@gmail.com>
2024-03-20 16:58:46 -0700

    client submodule

--- src/core/components/navigation/TransitionPage/index.js
+++ src/core/components/navigation/TransitionPage/index.js
@@ -1,4 +1,9 @@
 export { default as animations } from './animations';
+
+// Dynamically import the component because of the property touchScreenOnly
+// which evalutae if a device screen is touchable on client side.
+// Otherwise you get an error like:
+// Hydration failed because the initial UI does not match what was rendered on the server.
 import dynamic from 'next/dynamic';
 export const TransitionPage = dynamic(() => import('./TransitionPage'), {
   ssr: false,
