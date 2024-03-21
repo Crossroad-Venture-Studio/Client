047dcbe505fce1e3cdf4b70c7db15f0dae185155
William Brendel <brendel.william@gmail.com>
2024-03-20 18:13:16 -0700

    client submodule

--- src/core/components/navigation/TransitionPage/transitions.js
+++ src/core/components/navigation/TransitionPage/transitions.js
@@ -1,7 +1,7 @@
 import animations from './animations';
 
 export const transitions = {};
-const createTransition = name => pageTransitions[name] = () => animations.set(name);
+const createTransition = name => transitions[name] = () => animations.set(name);
 export const slideIn = createTransition('slideIn');
 export const slideOut = createTransition('slideOut');
 export const dissolve = createTransition('dissolve');
