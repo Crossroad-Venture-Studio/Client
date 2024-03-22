8e1aaec287a1fca9e6b2bfcab0d10d486e9ed66f
William Brendel <brendel.william@gmail.com>
2024-03-22 11:22:15 -0700

    client submodule

--- src/core/components/templates/index.js
+++ src/core/components/templates/index.js
@@ -1,2 +1,4 @@
 // Exports.
 export { default as Page } from './Page';
+export { default as UserAuthenticatedContainer } from './UserAuthenticatedContainer/UserAuthenticatedContainer';
+export { default as UserAuthentificationContainer } from './UserAuthentificationContainer/UserAuthentificationContainer';
