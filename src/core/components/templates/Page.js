912fd47bf46eb7477766b8097657e31a1ac213d0
William Brendel <brendel.william@gmail.com>
2024-04-04 23:05:40 -0700

    client submodule

--- src/core/components/templates/Page.js
+++ src/core/components/templates/Page.js
@@ -1,5 +1,10 @@
 'use client';
 
+// Imports.
+import Platform from '../../native/Platform';
+import { useEffect, useRef } from 'react';
+import debounce from '../../../../utils/src/debounce';
+
 // Main component.
 export const Page = props => {
   // Normalize input.
@@ -7,13 +12,22 @@ export const Page = props => {
     children,
     className,
     safearea,
+    observeScroll,
     ...other
   } = props || {};
   className = (className && `page ${className}`) || 'page';
   safearea && (className = `safearea ${className}`);
   Array.isArray(children || (children = [])) || (children = [children]);
+
+  // Detect scroll.
+  const ref = useRef();
+  observeScroll && useEffect(() => {
+    Platform.document.addEventListener('scroll', debounce(() => {
+      ref && ref.current && (ref.current.dataset.scrolling = window.scrollTop > 0);
+    }), { passive: true });
+  }, []);
   
-  return <main className={className} {...other}>
+  return <main className={className} ref={ref} {...other}>
     {...children}
   </main>
 }
