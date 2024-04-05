7c4a129541835332c52828a4be214bac72ab393d
William Brendel <brendel.william@gmail.com>
2024-04-05 16:10:59 -0700

    client submodule

--- src/utils/print.js
+++ src/utils/print.js
@@ -3,7 +3,7 @@ import Platform from '../core/native/Platform';
 
 // Helper function to trigger the pinter.
 export const print = () => {
-  const w = Platform.window; p = w && typeof w.print;
+  const w = Platform.window, p = w && typeof w.print;
   if (typeof p === 'function') {
     const pages = Platform.document.getElementsByClassName('page') || [];
     for (let i = 0, l = pages.length; i !== l; ++i) {
