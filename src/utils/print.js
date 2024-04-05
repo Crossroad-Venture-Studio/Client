a42ff6b6c3e7c9abcc1eb9a5c9349e8c16ccdf90
William Brendel <brendel.william@gmail.com>
2024-04-05 16:20:38 -0700

    client submodule

--- src/utils/print.js
+++ src/utils/print.js
@@ -5,13 +5,20 @@ import Platform from '../core/native/Platform';
 export const print = () => {
   const w = Platform.window, p = w && w.print;
   if (typeof p === 'function') {
-    const pages = Platform.document.getElementsByClassName('page') || [];
+    const pages = Platform.document.getElementsByClassName('page') || [],
+      topNavbars = Platform.document.getElementsByClassName('top-navbar') || [];
     for (let i = 0, l = pages.length; i !== l; ++i) {
       pages[i].classList.add('print');
+    }
+    for (let i = 0, l = topNavbars.length; i !== l; ++i) {
+      topNavbars[i].classList.add('print');
     }
     w.print();
     for (let i = 0, l = pages.length; i !== l; ++i) {
       pages[i].classList.remove('print');
+    }
+    for (let i = 0, l = topNavbars.length; i !== l; ++i) {
+      topNavbars[i].classList.remove('print');
     }
   }
 };
