86e1760e42254f878a531fdecbbd3eae899db679
William Brendel <brendel.william@gmail.com>
2024-04-05 16:09:10 -0700

    client submodule

---
+++ src/core/components/atoms/Spinner/index.js
@@ -0,0 +1,7 @@
+import styles from './spinner.module.css';
+
+// Main component.
+export const Spinner = () => <div className={`${styles['lds-ring']}`}><div></div><div></div><div></div><div></div></div>;
+
+// Exports.
+export default Spinner;
--- src/core/components/atoms/index.js
+++ src/core/components/atoms/index.js
@@ -6,4 +6,5 @@ export { default as Form } from './Form';
 export { default as NextButton } from './NextButton';
 export { default as Pill } from './Pill';
 export { default as Row } from './Row';
+export { default as Spinner } from './Spinner';
 export { default as SubmitButton } from './SubmitButton';
