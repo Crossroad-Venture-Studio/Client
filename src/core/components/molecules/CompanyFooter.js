1933dce1b4ac3acde88cff76f594c3b01575f301
William Brendel <brendel.william@gmail.com>
2024-04-04 21:21:54 -0700

    client submodule

---
+++ src/core/components/molecules/CompanyFooter.js
@@ -0,0 +1,50 @@
+// Imports
+import Button from '../atoms/Button';
+import Row from '../atoms/Row';
+import Column from '../atoms/Row';
+
+export const  CompanyFooter = props => {
+  let {
+    className,
+    logoSrc,
+    logo = logoSrc,
+    logoTitle = 'Company logo',
+    logoAlt = logoTitle,
+    text,
+    description = text,
+    value = description,
+    buttons,
+    rightNav = buttons,
+    socials,
+    copyright,
+    links,
+    ...other
+  } = props || {};
+  const baseClassName = `company-footer responsive spread gap center`;
+  className = className && `${baseClassName} ${className}` || baseClassName;
+  rightNav = (Array.isArray(rightNav) && rightNav || [rightNav]).filter(x => x);
+  socials = (Array.isArray(socials) && socials || [socials]).filter(x => x);
+  links = (Array.isArray(links) && links || [links]).filter(x => x);
+
+  // Render.
+  return <Row className={className} {...other}>
+    <Column className='text-gap fill font-size-12-14'>
+      {logo && <img src={logo} className='company-footer-logo icon' title={logoTitle} alt={logoAlt}/> || null}
+      {value && <span className='company-footer-description'>{value}</span> || null}
+    </Column>
+    <Column className='text-gap fill right font-size-12-14'>
+      {rightNav.length && <div className='row center'>
+        {...(rightNav.map((b, i) => <Button {...b} key={`${i}`}></Button>))}
+      </div> || null}
+      {socials.length && <div className='row center'>
+        {...(socials.map((b, i) => <Button {...b} key={`${i}`}></Button>))}
+      </div> || null}
+      {links.length && <div className='row center'>
+        {...(links.map((b, i) => <Button {...b} key={`${i}`}></Button>))}
+      </div> || null}
+    </Column>
+  </Row>;
+}
+
+// Default export.
+export default CompanyFooter;
