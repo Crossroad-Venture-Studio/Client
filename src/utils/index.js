692d9d4b0e54d582556e7f66efa835951945ef9a
William Brendel <brendel.william@gmail.com>
2024-04-10 21:31:27 -0700

    client submodule

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,4 +4,5 @@ export { default as createEventHandlers } from './createEventHandlers';
 export { default as createStopPropagationFunc } from './createStopPropagationFunc';
 export { default as normalizeEventHandler } from './normalizeEventHandler';
 export { default as print } from './print';
+export { default as setDynamicViewPortSize } from './setDynamicViewPortSize';
 export * from './color';
