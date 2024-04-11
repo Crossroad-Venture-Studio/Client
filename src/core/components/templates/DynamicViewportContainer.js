76fd0b784ea4e24198db3eda4cddc1e85949c375
William Brendel <brendel.william@gmail.com>
2024-04-10 21:41:45 -0700

    new commit

---
+++ src/core/components/templates/DynamicViewportContainer.js
@@ -0,0 +1,20 @@
+'use client';
+
+import setDynamicViewPortSize from '../../../utils/setDynamicViewPortSize';
+import { useEffect } from 'react';
+
+// App container.
+let hasViewportHandler;
+export const DynamicViewportContainer = ({children}) => {
+
+  useEffect(() => {
+    hasViewportHandler || (
+      setDynamicViewPortSize(),
+      hasViewportHandler = true
+    );
+  }, []);
+
+  return <>
+    {children}
+  </>;
+}
--- src/core/components/templates/index.js
+++ src/core/components/templates/index.js
@@ -1,2 +1,3 @@
 // Exports.
+export { default as DynamicViewportContainer } from './DynamicViewportContainer';
 export { default as Page } from './Page';
