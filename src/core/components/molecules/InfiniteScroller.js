fb28c73d4813687aa53ab4d07f17cf9253eadd52
William Brendel <brendel.william@gmail.com>
2024-04-12 01:59:13 -0700

    new commit

--- src/core/components/molecules/InfiniteScroller.js
+++ src/core/components/molecules/InfiniteScroller.js
@@ -118,7 +118,7 @@ const addAnimation = (ref, children, props) => {
 
   // Clone children, for the animation.
   return children.map((child, i) => (
-    React.cloneElement(child, { /*'aria-hidden': true,*/ key: `a-h-${i}` })
+    React.cloneElement(child, { 'aria-hidden': true, key: `a-h-${i}` })
   ));
 }
 
