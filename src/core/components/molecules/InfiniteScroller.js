8e00395ee14f3ebdf613221547f25a8a77261ea9
William Brendel <brendel.william@gmail.com>
2024-04-12 00:23:01 -0700

    new commit

--- src/core/components/molecules/InfiniteScroller.js
+++ src/core/components/molecules/InfiniteScroller.js
@@ -87,7 +87,7 @@ export const InfiniteScroller = props => {
   // Render.
   return <div className={className} ref={ref}>
     <div className='infinite-scroller-inner'>
-      <div className='gap-half'>{' '}</div>
+      <div style={{width: '1px', height: '1px'}}></div>
       {data}
       {children}
       {extraChildren}
