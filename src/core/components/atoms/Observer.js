aba7a97b789bfe0320aedee5e829f18d5cbc056a
William Brendel <brendel.william@gmail.com>
2024-04-23 12:16:55 -0700

    new commit

--- src/core/components/atoms/Observer.js
+++ src/core/components/atoms/Observer.js
@@ -12,7 +12,25 @@ export const Observer = props => {
     onRefresh,
     children
   } = props || {};
-  useObserver(obj, attr, onRefresh);
+  // useObserver(obj, attr, onRefresh);
+
+  const valid = obj && attr && obj.hasOwnProperty(attr);
+  let value = valid ? obj[attr] : null;
+  const [state, setState] = useState(value);
+  // const forceUpdate = useForceUpdate();
+  if (valid) {
+    useEffect(() => {
+      const refresh = typeof onRefresh === 'function' && (() => {
+        const v = obj[attr];
+        v !== state && onRefresh(v);
+        setState(v);
+        console.log('OBSERVED', state, v);
+        // forceUpdate();
+      }) || (() => setState(obj[attr]));
+      observe(obj, attr, refresh);
+    }, []);
+  }
+
   return <div className='observer' key={`${Math.random()}`}>{children}</div>;
 }
 
