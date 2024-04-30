a318a01259410a2882c18038eaa27c863a3593ac
William Brendel <brendel.william@gmail.com>
2024-04-30 11:54:37 -0700

    new commit

---
+++ src/core/components/atoms/Logo.js
@@ -0,0 +1,35 @@
+// Prominence logo.
+export const Logo = props => {
+  let {
+    type = 'color',
+    height = 32,
+    width = 'auto',
+    responsive,
+    title = 'Logo',
+    alt = title,
+    prefix,
+    dir = 'assets/logos',
+    src = `${dir}/${prefix && `${prefix}-` || ''}logo${type && `-${type}` || ''}.svg`,
+    ...other
+  } = props || {};
+  if (responsive) {
+    let v = parseFloat(height);
+    isNaN(v) || (height = `min(max(calc(${v} * 0.05vw), calc(0.5 * ${v}px), ${v}px)`);
+    v = parseFloat(width);
+    isNaN(v) || (width = `min(max(calc(${v} * 0.05vw), calc(0.5 * ${v}px), ${v}px)`);
+  } else {
+    parseFloat(height) == height && (height = `${height}px`);
+    parseFloat(width) == width && (width = `${width}px`);
+  }
+
+  // Render.
+  return <img
+    src={src}
+    style={{width, height}}
+    title={title || null}
+    alt={alt || null}
+    {...other}
+  />;
+}
+
+export default Logo;
