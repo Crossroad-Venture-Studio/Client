3057a34fcd4dc46ac0faf15f5db6918c52d60afb
William Brendel <brendel.william@gmail.com>
2024-05-01 09:53:38 -0700

    new commit

--- src/data/index.js
+++ src/data/index.js
@@ -1,5 +1,4 @@
 // Exports.
 export { default as DataWarehouse } from './DataWarehouse';
 export { default as observe } from './observe';
-export { default as Locales } from '../utils/locales/Locales';
-export { default as commonTranslations } from './commonTranslations';
+export { default as Store } from './Store';
--- src/utils/locales/index.js
+++ src/utils/locales/index.js
@@ -1,3 +1,4 @@
 export * from './localeUtils';
 export * from './Locales';
 export * from './TranslationTable';
+export { default as commonTranslations } from './commonTranslations';
