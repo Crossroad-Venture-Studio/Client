5407b82bbc3af338aac4f1e8f1d5c148846fb990
William Brendel <brendel.william@gmail.com>
2024-05-01 10:31:38 -0700

    new commit

--- src/utils/locales/commonTranslations.js
+++ src/utils/locales/commonTranslations.js
@@ -1,103 +1,103 @@
 export const commonTranslations = [
   {
-      en: 'Contact us',
-      es: 'Contáctenos'
+    en: 'Contact us',
+    es: 'Contáctenos'
   },
   {
-      en: 'About us',
-      es: 'Sobre nosotras'
+    en: 'About us',
+    es: 'Sobre nosotras'
   },
   {
-      en: 'Register',
-      es: 'Registrar'
+    en: 'Register',
+    es: 'Registrar'
   },
   {
-      en: 'Subscribe',
-      es: 'Suscribirse'
+    en: 'Subscribe',
+    es: 'Suscribirse'
   },
   {
-      en: 'Log in',
-      es: 'Iniciar sesión'
+    en: 'Log in',
+    es: 'Iniciar sesión'
   },
   {
-      en: 'Log out',
-      es: 'Cerrar sesión'
+    en: 'Log out',
+    es: 'Cerrar sesión'
   },
   {
-      en: 'Create account',
-      es: 'Crear cuenta'
+    en: 'Create account',
+    es: 'Crear cuenta'
   },
   {
-      en: 'Terms of use',
-      es: 'Condiciones de uso'
+    en: 'Terms of use',
+    es: 'Condiciones de uso'
   },
   {
-      en: 'Copyrights',
-      es: 'Derechos de autor'
+    en: 'Copyrights',
+    es: 'Derechos de autor'
   },
   {
-      en: 'All rights reserved',
-      es: 'Reservados todos los derechos'
+    en: 'All rights reserved',
+    es: 'Reservados todos los derechos'
   },
   {
-      en: 'Privacy policy',
-      es: 'Política de privacidad'
+    en: 'Privacy policy',
+    es: 'Política de privacidad'
   },
   {
-      en: 'I speak 🇺🇸',
-      es: 'Hablas 🇲🇽'
+    en: 'I speak 🇺🇸',
+    es: 'Hablas 🇲🇽'
   },
   {
-      en: '🇺🇸',
-      es: '🇲🇽'
+    en: '🇺🇸',
+    es: '🇲🇽'
   },
   {
-      en: 'I speak english',
-      es: 'Hablas español'
+    en: 'I speak english',
+    es: 'Hablas español'
   },
   {
-      en: 'Free demo',
-      es: 'Prueba gratis'
+    en: 'Free demo',
+    es: 'Prueba gratis'
   },
   {
-      en: 'Free trial',
-      es: 'Prueba gratis'
+    en: 'Free trial',
+    es: 'Prueba gratis'
   },
   {
-      en: 'Book demo',
-      es: 'Reservar una demostración'
+    en: 'Book demo',
+    es: 'Reservar una demostración'
   },
   {
-      en: 'Book a demo',
-      es: 'Reservar una demostración'
+    en: 'Book a demo',
+    es: 'Reservar una demostración'
   },
   {
-      en: 'Meet us',
-      es: 'Conócenos'
+    en: 'Meet us',
+    es: 'Conócenos'
   },
   {
-      en: 'Book a meeting with us',
-      es: 'Reserve una reunión con nosotros'
+    en: 'Book a meeting with us',
+    es: 'Reserve una reunión con nosotros'
   },
   {
-      en: 'Book a meeting',
-      es: 'Reserve una reunión'
+    en: 'Book a meeting',
+    es: 'Reserve una reunión'
   },
   {
-      en: 'Send message',
-      es: 'Enviar mensaje'
+    en: 'Send message',
+    es: 'Enviar mensaje'
   },
   {
-      en: 'Send',
-      es: 'Enviar'
+    en: 'Send',
+    es: 'Enviar'
   },
   {
-      en: 'Submit form',
-      es: 'Enviar formulario'
+    en: 'Submit form',
+    es: 'Enviar formulario'
   },
   {
-      en: 'Submit',
-      es: 'Enviar'
+    en: 'Submit',
+    es: 'Enviar'
   },
   {
     en: 'Settings',
@@ -119,6 +119,122 @@ export const commonTranslations = [
     en: 'Profile',
     es: 'Perfil'
   },
+  {
+    en: 'Name',
+    es: 'Nombre'
+  },
+  {
+    en: 'First name',
+    es: 'Nombre de pila'
+  },
+  {
+    en: 'Last name',
+    es: 'Apellido'
+  },
+  {
+    en: 'Middle name',
+    es: 'Segundo nombre'
+  },
+  {
+    en: 'Mail',
+    es: 'Correo'
+  },
+  {
+    en: 'Email',
+    es: 'Correo electrónico'
+  },
+  {
+    en: 'E-mail',
+    es: 'Correo electrónico'
+  },
+  {
+    en: 'Email address',
+    es: 'Correo electrónico'
+  },
+  {
+    en: 'Subject',
+    es: 'Asunto'
+  },
+  {
+    en: 'Comment',
+    es: 'Comentario'
+  },
+  {
+    en: 'Comments',
+    es: 'Comentarios'
+  },
+  {
+    en: 'Phone number',
+    es: 'Número de teléfono'
+  },
+  {
+    en: 'Mobile number',
+    es: 'Número de teléfono móvil'
+  },
+  {
+    en: 'Fax number',
+    es: 'Número de fax'
+  },
+  {
+    en: 'Number',
+    es: 'Número'
+  },
+  {
+    en: 'Street number',
+    es: 'Número de calle'
+  },
+  {
+    en: 'Apartment',
+    es: 'Departamento'
+  },
+  {
+    en: 'Suite',
+    es: 'habitación'
+  },
+  {
+    en: 'Unit',
+    es: 'Unidad'
+  },
+  {
+    en: 'Building',
+    es: 'Edificio'
+  },
+  {
+    en: 'Floor',
+    es: 'piso'
+  },
+  {
+    en: 'Address',
+    es: 'Dirección'
+  },
+  {
+    en: 'Street',
+    es: 'Calle'
+  },
+  {
+    en: 'Zipcode',
+    es: 'Código postal'
+  },
+  {
+    en: 'Zip code',
+    es: 'Código postal'
+  },
+  {
+    en: 'County',
+    es: 'Condado'
+  },
+  {
+    en: 'Region',
+    es: 'Región'
+  },
+  {
+    en: 'State',
+    es: 'estado'
+  },
+  {
+    en: 'Country',
+    es: 'País'
+  }
 ];
 
 export default commonTranslations;
