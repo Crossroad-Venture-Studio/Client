a60d02603879638700697f78a0043b1c23719c56
William Brendel <brendel.william@gmail.com>
2024-05-01 10:12:46 -0700

    new commit

--- src/utils/locales/TranslationTable.js
+++ src/utils/locales/TranslationTable.js
@@ -54,10 +54,6 @@ export class TranslationTable {
     locale = normalizeLocale(locale);
     let [index, transform] = this.map.get(text) || [], s;
 
-    console.log('>>>>', text, this, index, transform, locale, index !== undefined && (s = this.table[index][locale]) !== undefined && (
-      transform && s[transform]() || s
-    ) || text);
-
     // Return translation.
     return index !== undefined && (s = this.table[index][locale]) !== undefined && (
       transform && s[transform]() || s
