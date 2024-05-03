5a46bdf26938a24dd12b99cf9862f4cf8fb32c54
William Brendel <brendel.william@gmail.com>
2024-05-02 17:00:34 -0700

    new commit

--- src/utils/locales/commonTranslations.js
+++ src/utils/locales/commonTranslations.js
@@ -1,279 +1,507 @@
 export const commonTranslations = [
   {
     en: 'Contact',
-    es: 'Contacto'
+    es: 'Contacto',
+    fr: 'Contact',
+    pt: 'Contato',
   },
   {
     en: 'Contacts',
-    es: 'Contactos'
+    es: 'Contactos',
+    en: 'Contacts',
+    pt: 'Contatos',
   },
   {
     en: 'Contact us',
-    es: 'Contáctenos'
+    es: 'Contáctenos',
+    fr: 'Contactez nous',
+    pt: 'Contate-nos',
   },
   {
     en: 'About us',
-    es: 'Sobre nosotras'
+    es: 'Sobre nosotras',
+    fr: 'À propos de nous',
+    pt: 'Sobre nós'
   },
   {
     en: 'Register',
-    es: 'Registrar'
+    es: 'Registrar',
+    fr: 'S\'enregistrer',
+    pt: 'Registrar'
   },
   {
     en: 'Subscribe',
-    es: 'Suscribirse'
+    es: 'Suscribirse',
+    fr: 'S\'abonner',
+    pt: 'Se inscrever'
   },
   {
     en: 'Log in',
-    es: 'Iniciar sesión'
+    es: 'Conectarse',
+    fr: 'Se connecter',
+    pt: 'Conecte-se'
   },
   {
     en: 'Log out',
-    es: 'Cerrar sesión'
+    es: 'Desconectarse',
+    fr: 'Se connecter',
+    pt: 'Deconecte-se'
+  },
+  {
+    en: 'Account',
+    es: 'Cuenta',
+    fr: 'Compte',
+    pt: 'Conta'
   },
   {
     en: 'Create account',
-    es: 'Crear cuenta'
+    es: 'Crear cuenta',
+    fr: 'Créer compte',
+    pt: 'Criar conta'
+  },
+  {
+    en: 'Your account',
+    es: 'Su cuenta',
+    fr: 'Votre compte',
+    pt: 'Sua conta'
+  },
+  {
+    en: 'User account',
+    es: 'Cuenta de usuario',
+    fr: 'Compte utilisateur',
+    pt: 'Conta do usuário'
   },
   {
     en: 'Terms of use',
-    es: 'Condiciones de uso'
-  },
-  {
-    en: 'Copyrights',
-    es: 'Derechos de autor'
-  },
-  {
-    en: 'All rights reserved',
-    es: 'Reservados todos los derechos'
+    es: 'Condiciones de uso',
+    fr: 'Conditions Générales d\'Utilisation',
+    pt: 'Condições de uso'
   },
   {
     en: 'Privacy policy',
-    es: 'Política de privacidad'
+    es: 'Política de privacidad',
+    fr: 'Politique d\'Utilisation de données privées',
+    pt: 'Política de privacidade'
+  },
+  {
+    en: 'Copyrights',
+    es: 'Derechos de autor',
+    fr: 'Droits d\'auteur',
+    pt: 'Direitos de autor'
+  },
+  {
+    en: 'All rights reserved',
+    es: 'Reservados todos los derechos',
+    fr: 'Tous droits réservés',
+    pt: 'Reservados todos os direitos'
   },
   {
     en: 'I speak 🇺🇸',
-    es: 'Hablo 🇲🇽'
+    es: 'Hablo 🇪🇸',
+    fr: 'Je parle 🇫🇷',
+    pt: 'Hablo 🇵🇹'
   },
   {
     en: '🇺🇸',
-    es: '🇲🇽'
+    es: '🇪🇸',
+    fr: '🇫🇷',
+    pt: '🇵🇹'
   },
   {
     en: 'I speak English',
-    es: 'Hablo español'
+    es: 'Hablo español',
+    fr: 'Je parle français',
+    pt: 'Hablo português'
   },
   {
     en: 'English',
-    es: 'Español'
+    es: 'Español',
+    fr: 'Français',
+    pt: 'Português'
   },
   {
     en: 'Switch language',
-    es: 'Cambiar idioma'
+    es: 'Cambiar idioma',
+    fr: 'Changer de langue',
+    pt: 'Mudar idioma'
   },
   {
     en: 'Free demo',
-    es: 'Prueba gratis'
+    es: 'Prueba gratis',
+    fr: 'Demo gratuite',
+    pt: 'Demo gratuita'
   },
   {
     en: 'Free trial',
-    es: 'Prueba gratis'
+    es: 'Prueba gratis',
+    fr: 'Essai gratuit',
+    pt: 'Teste grátis'
   },
   {
     en: 'Book demo',
-    es: 'Reservar una demostración'
+    es: 'Reservar una demo',
+    fr: 'Demander une démo',
+    pt: 'Agende demo'
   },
   {
     en: 'Book a demo',
-    es: 'Reservar una demostración'
+    es: 'Reservar una demo',
+    fr: 'Demander une démo',
+    pt: 'Agende demo'
+  },
+  {
+    en: 'Ask for a demo',
+    es: 'Reservar una demo',
+    fr: 'Demander une démo',
+    pt: 'Peça uma demo'
+  },
+  {
+    en: 'Talk to us',
+    es: 'Habla con nosotros',
+    fr: 'Parlez-nous',
+    pt: 'Habla conosco'
   },
   {
     en: 'Meet us',
-    es: 'Conócenos'
+    es: 'Conócenos',
+    fr: 'Rencontrez-nous',
+    pt: 'Encontre-nos'
   },
   {
     en: 'Book a meeting with us',
-    es: 'Reserve una reunión con nosotros'
+    es: 'Hacer una cita con nosotros',
+    fr: 'Prendre un rendez-vous avec nous',
+    pt: 'Faça um encontro conosco'
   },
   {
     en: 'Book a meeting',
-    es: 'Reserve una reunión'
+    es: 'Hacer una cita ',
+    fr: 'Prendre rendez-vous',
+    pt: 'Faça um encontro'
   },
   {
     en: 'Send message',
-    es: 'Enviar mensaje'
+    es: 'Enviar mensaje',
+    fr: 'Envoyer message',
+    pt: 'Enviar mensagem'
   },
   {
     en: 'Send',
-    es: 'Enviar'
+    es: 'Enviar',
+    fr: 'Envoyer',
+    pt: 'Enviar'
   },
   {
     en: 'Submit form',
-    es: 'Enviar formulario'
+    es: 'Enviar formulario',
+    fr: 'Soumettre formulaire',
+    pt: 'Enviar formulário'
   },
   {
     en: 'Submit',
-    es: 'Enviar'
+    es: 'Enviar',
+    fr: 'Soumettre',
+    pt: 'Enviar'
   },
   {
     en: 'Settings',
-    es: 'Ajustes'
+    es: 'Ajustes',
+    fr: 'Paramètres',
+    pt: 'Configurações'
   },
   {
     en: 'User',
-    es: 'Usuario'
+    es: 'Usuario',
+    fr: 'Utilisateur',
+    pt: 'Usuário'
   },
   {
     en: 'Users',
-    es: 'Usuarios'
+    es: 'Usuarios',
+    fr: 'Utilisateurs',
+    pt: 'Usuários'
+  },
+  {
+    en: 'Friend',
+    es: 'Amigo',
+    fr: 'Ami',
+    pt: 'Amigo'
+  },
+  {
+    en: 'Friends',
+    es: 'Amigos',
+    fr: 'Amis',
+    pt: 'Amigos'
+  },
+  {
+    en: 'Invite friends',
+    es: 'Invitar amigos',
+    fr: 'Inviter amis',
+    pt: 'Convidar amigos'
+  },
+  {
+    en: 'Invite a friend',
+    es: 'Invitar un amigo',
+    fr: 'Inviter un ami',
+    pt: 'Convidar un amigo'
   },
   {
     en: 'User profile',
-    es: 'Perfil del usuario'
+    es: 'Perfil usuario',
+    fr: 'Profil utilisateur',
+    pt: 'Perfil usuário'
   },
   {
     en: 'Profile',
-    es: 'Perfil'
+    es: 'Perfil',
+    fr: 'Profil',
+    pt: 'Perfil'
   },
   {
     en: 'Name',
-    es: 'Nombre'
+    es: 'Nombre',
+    fr: 'Nom',
+    pt: 'Nome'
   },
   {
     en: 'First name',
-    es: 'Nombre de pila'
+    es: 'Nombre de pila',
+    fr: 'Prénom',
+    pt: 'Nome'
   },
   {
     en: 'Last name',
-    es: 'Apellido'
+    es: 'Apellido',
+    fr: 'Nom de famille',
+    pt: 'Sobrenome'
   },
   {
     en: 'Middle name',
-    es: 'Segundo nombre'
+    es: 'Segundo nombre',
+    fr: 'Deuxième prénom',
+    pt: 'Nome do meio'
+  },
+  {
+    en: 'Initials',
+    es: 'Iniciales',
+    fr: 'Initiales',
+    pt: 'Iniciais'
   },
   {
     en: 'Mail',
-    es: 'Correo'
+    es: 'Correo',
+    fr: 'Courier',
+    pt: 'Correo'
   },
   {
     en: 'Email',
-    es: 'Correo electrónico'
+    es: 'Correo electrónico',
+    fr: 'Couriel',
+    pt: 'Correio eletrônico'
   },
   {
     en: 'E-mail',
-    es: 'Correo electrónico'
+    es: 'Correo electrónico',
+    fr: 'Couriel',
+    pt: 'Correio eletrônico'
   },
   {
     en: 'Email address',
-    es: 'Correo electrónico'
+    es: 'Correo electrónico',
+    fr: 'Adresse électronique',
+    pt: 'Correio eletrônico'
   },
   {
     en: 'E-mail address',
-    es: 'Correo electrónico'
+    es: 'Correo electrónico',
+    fr: 'Adresse électronique',
+    pt: 'Correio eletrônico'
   },
   {
     en: 'Subject',
-    es: 'Asunto'
+    es: 'Sujeto',
+    fr: 'Sujet',
+    pt: 'Sujeito'
   },
   {
     en: 'Type your message here',
-    es: 'Escribe tu mensaje aquí'
+    es: 'Escribe tu mensaje aquí',
+    fr: 'tapez votre message ici',
+    pt: 'Digite sua mensagem aqu'
   },
   {
     en: 'Comment',
-    es: 'Comentario'
+    es: 'Comentario',
+    fr: 'Commentaire',
+    pt: 'Comentário'
   },
   {
     en: 'Comments',
-    es: 'Comentarios'
+    es: 'Comentarios',
+    fr: 'Commentaires',
+    pt: 'Comentários'
   },
   {
     en: 'Request',
-    es: 'Pedido'
+    es: 'Pedido',
+    fr: 'Demande',
+    pt: 'Pedido'
   },
   {
     en: 'Requests',
-    es: 'Peticiones'
+    es: 'Pedidos',
+    fr: 'Demandes',
+    pt: 'Pedidos'
+  },
+  {
+    en: 'Offer',
+    es: 'Oferta',
+    fr: 'Offre',
+    pt: 'Oferta'
+  },
+  {
+    en: 'Offers',
+    es: 'Ofertas',
+    fr: 'Offres',
+    pt: 'Ofertas'
+  },
+  {
+    en: 'Send request',
+    es: 'Enviar pedido',
+    fr: 'Envoyer votre demande',
+    pt: 'Enviar pedido'
+  },
+  {
+    en: 'Send requests',
+    es: 'Enviar pedidos',
+    fr: 'Envoyer vos demandes',
+    pt: 'Enviar pedidos'
   },
   {
     en: 'Order',
-    es: 'Pedido'
+    es: 'Pedido',
+    fr: 'Commande',
+    pt: 'Pedido'
   },
   {
     en: 'Orders',
-    es: 'Pedidos'
+    es: 'Pedidos',
+    es: 'Commandes',
+    pt: 'Pedidos'
   },
   {
     en: 'Phone number',
-    es: 'Número de teléfono'
+    es: 'Número de teléfono',
+    fr: 'Numéro de téléphone',
+    pt: 'Número de telefone'
   },
   {
     en: 'Mobile number',
-    es: 'Número de teléfono móvil'
+    es: 'Número de teléfono móvil',
+    fr: 'Numéro de téléphone portable',
+    pt: 'Número de telemóvel'
+  },
+  {
+    en: 'Mobile',
+    es: 'Móvil',
+    fr: 'Portable',
+    pt: 'Móvel'
   },
   {
     en: 'Fax number',
-    es: 'Número de fax'
+    es: 'Número de fax',
+    fr: 'Numéro de fax',
+    pt: 'Número de fax'
   },
   {
     en: 'Number',
-    es: 'Número'
+    es: 'Número',
+    fr: 'Numéro',
+    pt: 'Número'
   },
   {
     en: 'Street number',
-    es: 'Número de calle'
+    es: 'Número de calle',
+    fr: 'Numéro de rue',
+    pt: 'Número da rua'
   },
   {
     en: 'Apartment',
-    es: 'Departamento'
+    es: 'Departamento',
+    fr: 'Appartement',
+    pt: 'Apartamento'
   },
   {
     en: 'Suite',
-    es: 'habitación'
+    es: 'Habitación',
+    fr: 'Suite',
+    pt: 'Habitação'
   },
   {
     en: 'Unit',
-    es: 'Unidad'
+    es: 'Número',
+    fr: 'Numéro',
+    pt: 'Número'
   },
   {
     en: 'Building',
-    es: 'Edificio'
+    es: 'Edificio',
+    fr: 'Batiment',
+    pt: 'Prédio'
   },
   {
     en: 'Floor',
-    es: 'piso'
+    es: 'Planta',
+    fr: 'Étage',
+    pt: 'Andar'
   },
   {
     en: 'Address',
-    es: 'Dirección'
+    es: 'Dirección',
+    fr: 'Adresse',
+    pt: 'Endereço'
   },
   {
     en: 'Street',
-    es: 'Calle'
+    es: 'Calle',
+    fr: 'Rue',
+    pt: 'Rua'
   },
   {
     en: 'Zipcode',
-    es: 'Código postal'
+    es: 'Código postal',
+    fr: 'Code postal',
+    pt: 'CEP'
   },
   {
     en: 'Zip code',
-    es: 'Código postal'
+    es: 'Código postal',
+    fr: 'Code postal',
+    pt: 'CEP'
   },
   {
     en: 'County',
-    es: 'Condado'
+    es: 'Condado',
+    fr: 'Comté',
+    pt: 'Condado'
   },
   {
     en: 'Region',
-    es: 'Región'
+    es: 'Región',
+    fr: 'Région',
+    pt: 'Região'
   },
   {
     en: 'State',
-    es: 'estado'
+    es: 'Estado',
+    fr: 'État',
+    pt: 'Estado'
   },
   {
     en: 'Country',
-    es: 'País'
+    es: 'País',
+    fr: 'Pays',
+    pt: 'País'
   }
 ];
 
