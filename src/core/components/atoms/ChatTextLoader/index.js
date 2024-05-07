1d5aecc8d9afd05c3904df38b669babc46fe1d3f
William Brendel <brendel.william@gmail.com>
2024-05-07 11:11:07 -0700

    new commit

--- src/core/components/atoms/ChatTextLoader/index.js
+++ src/core/components/atoms/ChatTextLoader/index.js
@@ -2,7 +2,18 @@
 import styles from './chat-text-loader.module.css';
 
 // Main component.
-export const ChatTextLoader = () => <span className={`${styles['loader']}`}></span>;
+export const ChatTextLoader = props => {
+  let props = {
+    className,
+    type,
+    size = type
+  } = props || {},
+  baseClassName = styles['loader'];
+  className = className && `${baseClassName} ${className}` || baseClassName;
+  size && (className += ` ${size}`);
+
+  return <span className={className}></span>;
+}
 
 // Exports.
 export default ChatTextLoader;
