9d59d5c9e15110e000cbdf58642e2b93d5a35060
William Brendel <brendel.william@gmail.com>
2024-05-22 09:50:31 -0700

    new commit

--- src/core/components/modals/OverlayMenu.js
+++ src/core/components/modals/OverlayMenu.js
@@ -69,10 +69,14 @@ export const OverlayMenu = props => {
         className='width-100-percent row right close'
         src={closeSrc} onPress={() => hooks.close()}
         disabled={!isOpened}
-        key='close'
-      >yo</Button>
-      {...(nav.map(({src, icon, iconSrc, ...other} = {}, i) => <Button disabled={!isOpened} {...other} key={`${i}`}></Button>))}
+      />
+      {...(nav.map(({src, icon, iconSrc, ...other} = {}, i) => <Button disabled={!isOpened} {...other} key={`${i}`} />))}
       {...children}
+      <Button
+        className='width-100-percent row right close'
+        src={closeSrc} onPress={() => hooks.close()}
+        disabled={!isOpened}
+      />
     </div> || null}
   </Overlay>;
 }
