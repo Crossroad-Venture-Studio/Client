cdf4cb4044fdb900b070bba3840ebd32d88b62ae
William Brendel <brendel.william@gmail.com>
2024-05-22 21:28:59 -0700

    new commit

--- src/utils/chatbot/initiateNewConversation.js
+++ src/utils/chatbot/initiateNewConversation.js
@@ -16,6 +16,7 @@ export const initiateNewConversation = ({
   const history = store.data[conversationHistoryKey] || [];
   console.log('initiateNewConversation', history, history.length);
   // Create a conversation ID on initial load.
+  console.log('reset', reset, 'store', store, 'conversationIdKey', conversationIdKey);
   (!reset && store.data[conversationIdKey]) || (store.data[conversationIdKey] = createId());
 
   // Send first message.
