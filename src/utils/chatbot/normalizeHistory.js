c34da71a0d127bb609c5f6204de3aebb13aae8ea
William Brendel <brendel.william@gmail.com>
2024-05-23 15:24:01 -0700

    new commit

--- src/utils/chatbot/normalizeHistory.js
+++ src/utils/chatbot/normalizeHistory.js
@@ -43,14 +43,15 @@ export const normalizeHistory = (history, botName = 'Otto') => {
   // Add bubbles
   output.length && (
     output[0].__is_start__ = true,
-    output[output.length - 1].__is_end__ = true,
+    // output[output.length - 1].__is_end__ = true,
     output[output.length - 1].__is_user__ && output.push({
       component: <ChatBubble><ChatTextLoader size='xsmall'/></ChatBubble>,
       name: botName,
       date: Date.now(),
       // __is_start__: true,
-      __is_end__: true
-    })
+      // __is_end__: true
+    }),
+    output[output.length - 1].__is_end__ = true
   ) || (
     date = Date.now(),
     d = getDate(date),
