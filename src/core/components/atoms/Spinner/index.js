318cef605618c7b596e63a2a11479477beffe388
William Brendel <brendel.william@gmail.com>
2024-06-05 14:11:06 -0700

    new commit

--- src/core/components/atoms/Spinner/index.js
+++ src/core/components/atoms/Spinner/index.js
@@ -2,7 +2,26 @@
 import styles from './spinner.module.css';
 
 // Main component.
-export const Spinner = () => <div className={`${styles['lds-ring']}`}><div></div><div></div><div></div><div></div></div>;
+export const Spinner = props => {
+  let {
+    className,
+    large,
+    size,
+    light,
+    dark,
+    color
+  } = props || {}, baseClassName = `${styles['spinner']}`;
+  className = className && `${baseClassName} ${className}` || baseClassName;
+  (color || (dark && (color = 'dark')) || (light && (color = 'light'))) && (className += ` ${color}`);
+  (size || (large && (size = 'large'))) && (className += ` ${size}`);
+
+  return <div className={className}>
+    <div/>
+    <div/>
+    <div/>
+    <div/>
+  </div>;
+}
 
 // Exports.
 export default Spinner;
