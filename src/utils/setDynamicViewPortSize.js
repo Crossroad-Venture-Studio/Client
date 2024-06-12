002096d27117371a21b990f5fc8b9ed59fd76c47
William Brendel <brendel.william@gmail.com>
2024-06-12 16:11:19 -0700

    new commit

--- src/utils/setDynamicViewPortSize.js
+++ src/utils/setDynamicViewPortSize.js
@@ -38,8 +38,8 @@ const visualViewportResizeHandler = throttle(() => {
     document.documentElement.style.setProperty('--visual-viewport-offset-top', `${vvot || 0}px`);
     document.documentElement.style.setProperty('--visual-viewport-offset-left', `${vvol || 0}px`);
     document.documentElement.style.setProperty('--visual-viewport-offset-left', `${vvol || 0}px`);
-    document.documentElement.style.setProperty('--viewport-width-scale', `${vvw || 0}px`);
-    document.documentElement.style.setProperty('--viewport-height-scale', `${vvh || 0}px`);
+    document.documentElement.style.setProperty('--viewport-width-scaled', `${vvw || 0}px`);
+    document.documentElement.style.setProperty('--viewport-height-scaled', `${vvh || 0}px`);
     document.body.dataset.viewportScrolled = (vvt || vvot) && true || null;
     document.getElementById('chat-input').setAttribute('placeholder', `${Math.round(100 * document.documentElement.style.getPropertyValue('--vsy')) / 100} | ${vvh} + ${vvt} = ${h} | ${getComputedStyle(document.documentElement).getPropertyValue('--viewport-height-scaled')}`);
 }, 10);
