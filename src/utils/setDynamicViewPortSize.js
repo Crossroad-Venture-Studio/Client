154af7a15f125581fdc97b6a99b0738f6fddfa0c
William Brendel <brendel.william@gmail.com>
2024-06-11 21:29:12 -0700

    new commit

--- src/utils/setDynamicViewPortSize.js
+++ src/utils/setDynamicViewPortSize.js
@@ -30,18 +30,12 @@ const visualViewportResizeHandler = () => {
     vvol = clamp(vv.offsetLeft, 0, lvl - vvw);
 
   console.log('page', vvl, vvt);
-  // document.body.dataset.viewportResizing = (vvt || vvot) && true || null;
-  document.body.dataset.viewportResizing = window.navigator.virtualKeyboard && true || false;
-
-  // if ('virtualKeyboard' in navigator) {
-  //   navigator.virtualKeyboard.overlaysContent = true;
-  //   document.body.dataset.viewportResizing = true;
-  // }
+  document.body.dataset.viewportResizing = (vvt || vvot) && true || null;
   // document.documentElement.style.setProperty('--vvt', vvt && `${vvt}px` || '0');
   // document.documentElement.style.setProperty('--vvl', vvl && `${vvl}px` || '0');
   // document.documentElement.style.setProperty('--vvot', vvot && `${vvt}px` || '0');
   // document.documentElement.style.setProperty('--vvol', vvol && `${vvl}px` || '0');
-  // Platform.metaViewport.setAttribute && VVH !== h && Platform.metaViewport.setAttribute('content', `${META_VIEWPORT_CONTENT} height=${h}px`);
+  Platform.metaViewport.setAttribute && Platform.metaViewport.setAttribute('content', `${META_VIEWPORT_CONTENT} height=${h - vvt}px`);
   
 }
 
