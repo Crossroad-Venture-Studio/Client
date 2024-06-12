4a83c2609cf513cec2421b16b7d18ca67dc5bf7d
William Brendel <brendel.william@gmail.com>
2024-06-12 13:23:50 -0700

    new commit

--- src/utils/setDynamicViewPortSize.js
+++ src/utils/setDynamicViewPortSize.js
@@ -50,6 +50,7 @@ const visualViewportResizeHandler = () => {
   // }
   // window.addEventListener('scrollend', scrollEndEventHandler);
   // window.scrollTo(0,0);
+  window.scrollTop = window.scrollHeight;
 }
 
 let CNT = 0;
