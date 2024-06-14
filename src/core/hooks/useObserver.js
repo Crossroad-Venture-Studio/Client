cd27c38b4340c61e3bc30ff72bf71c3ad1d608d3
William Brendel <brendel.william@gmail.com>
2024-06-13 23:05:13 -0700

    new commit

--- src/core/hooks/useObserver.js
+++ src/core/hooks/useObserver.js
@@ -1,12 +1,11 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import observe from '../../data/observe';
 import '../../../utils/src/functionUtils';
 import useForceUpdate from './useForceUpdate';
 
 // Main hook.
-
 export function useObserver(obj, attr, onRefresh) {
   const valid = obj && attr && obj.hasOwnProperty(attr),
     value = valid ? obj[attr] : null,
@@ -20,71 +19,6 @@ export function useObserver(obj, attr, onRefresh) {
   }, []);
   return [value, setValue];
 }
-
-// export function useObserver(obj, attr, onRefresh) {
-//   const valid = obj && attr && obj.hasOwnProperty(attr);
-//   let value = valid ? obj[attr] : null;
-
-//   return [value, v => obj[attr] = v];
-
-//   // const [state, setState] = useState(value);
-//   const forceUpdate = useForceUpdate();
-
-//   useEffect(() => {
-//     const refresh = typeof onRefresh === 'function' && (() => {
-//       const v = obj[attr];
-//       v !== value && onRefresh(v);
-//       forceUpdate();
-//     }) || (() => forceUpdate());
-//     observe(obj, attr, refresh);
-//   }, []);
-
-//   return [value, v => obj[attr] = v];
-
-//   useEffect(() => {
-//     // const refresh = typeof onRefresh === 'function' && (() => {
-//     //   const v = obj[attr];
-//     //   v !== state && onRefresh(v);
-//     //   setState(v);
-//     //   forceUpdate();
-//     // }) || (() => setState(obj[attr]));
-//     const refresh = () => {
-//       setState(obj[attr])
-//     };
-//     observe(obj, attr, refresh);
-//   }, []);
-//   return [state, v => {
-//     // setState(obj[attr] = v);
-//   }];
-
-//   return [state, setState];
-
-//   useEffect(() => {
-//     const refresh = typeof onRefresh === 'function' && (() => {
-//       const v = obj[attr];
-//       v !== state && onRefresh(v);
-//       setState(v);
-//       forceUpdate();
-//     }) || (() => setState(obj[attr]));
-//     observe(obj, attr, refresh);
-//   }, []);
-//   return [state, v => obj[attr] = v];
-
-//   // if (valid) {
-//   //   useEffect(() => {
-//   //     const refresh = typeof onRefresh === 'function' && (() => {
-//   //       const v = obj[attr];
-//   //       v !== state && onRefresh(v);
-//   //       setState(v);
-//   //       forceUpdate();
-//   //     }) || (() => setState(obj[attr]));
-//   //     observe(obj, attr, refresh);
-//   //   }, []);
-//   //   return [state, v => obj[attr] = v];
-//   // }
-
-//   // return [state, setState];
-// }
 
 // Default export.
 export default useObserver;
