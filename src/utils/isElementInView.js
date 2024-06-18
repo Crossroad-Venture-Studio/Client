50fbc966631a0d181c771ed3c5ff2da565e2fa29
William Brendel <brendel.william@gmail.com>
2024-06-18 14:22:42 -0700

    new commit

--- src/utils/isElementInView.js
+++ src/utils/isElementInView.js
@@ -2,12 +2,6 @@ import Platform from '../core/native/Platform';
 
 export const isElementInView = (el, view) => {
   const rect = el.getBoundingClientRect();
-  if (rect.width === 0 && el.clientWidth) {
-    rect.left = el.offsetLeft;
-    rect.right = rect.left + el.clientWidth;
-    rect.top = el.offsetTop;
-    rect.bottom = rect.top + el.clientHeight;
-  }
   (view || (view = Platform)).getBoundingClientRect && (view = view.getBoundingClientRect());
   console.log(rect, view);
   return (
